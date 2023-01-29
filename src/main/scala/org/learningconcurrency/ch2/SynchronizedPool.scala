0620c5cca7ffc5999adf0534b366c9b1e5116434
19842639 <urzhenaliev@sberbank.ru>
2023-01-29 16:05:00 +0300

    termination

--- src/main/scala/org/learningconcurrency/ch2/SynchronizedPool.scala
+++ src/main/scala/org/learningconcurrency/ch2/SynchronizedPool.scala
@@ -1,18 +1,28 @@
 package org.learningconcurrency.ch2
 import scala.collection.mutable
+import scala.annotation.tailrec
 
 object SynchronizedPool extends App{
   private val tasks = mutable.Queue[() => Unit]()
 
   object Worker extends Thread {
     setDaemon(true)
+    var terminated = false
     def poll() = tasks.synchronized {
-        while (tasks.isEmpty) tasks.wait()
-        tasks.dequeue()
+        while (tasks.isEmpty && !terminated) tasks.wait()
+
+        if (!terminated) Some(tasks.dequeue) else None
     }
-    override def run() = while(true) {
-        val task = poll()
-        task()
+
+    @tailrec
+    override def run() = poll() match {
+        case Some(task) => task(); run()
+        case None => 
+    }
+
+    def shutdown() = tasks.synchronized {
+        terminated = true
+        tasks.notify
     }
   }
 
